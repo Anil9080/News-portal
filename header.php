22379166aeda36d97fbca243d28510071b61fdea
Anil9080 <49261723+Anil9080@users.noreply.github.com>
2020-10-23 08:40:01 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,25 @@
+ <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="brand"><h4 style="color:#7ca5c4; font-style: italic; font-size: 30px;">
+          <span style="color: pink;">TATHYA</span>DARPAN</h4></a>
+
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a  style="color:#fff; font-style: italic;"class="nav-link" href="about-us.php">About</a>
+            </li>
+                 <li class="nav-item">
+              <a style="color:#fff; font-style: italic;"class="nav-link" href="team.php">Team</a>
+            </li>
+             <li class="nav-item">
+              <a  style="color:#fff; font-style: italic;"class="nav-link" href="contact-us.php">Contact us</a>
+            </li>
+  
+  
+          </ul>
+        </div>
+      </div>
+    </nav>
